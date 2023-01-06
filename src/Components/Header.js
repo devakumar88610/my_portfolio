bc449c136e620c27a907769a79c6f8afb5106a32
devakumar88610 <devakumar88610@gmail.com>
2023-01-06 14:24:56 +0530

    okie

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import NavLink from './NavLink'
+import React, { useEffect, useState } from 'react';
+import NavLink from './NavLink';
 import NavMob from './NavMob';
 
 // Logo import
-import LogoWhite from "../assets/Images/Logo-white.svg"
-import LogoColor from "../assets/Images/Logo-color.svg"
+import LogoColor from "../assets/Images/Logo-color.svg";
+import LogoWhite from "../assets/Images/Logo-white.svg";
 
 const Header = () => {
     const [bg, setBg] = useState(false);
@@ -27,32 +27,32 @@ const Header = () => {
         <nav className={`${bg ? 'bg-white ' : ' bg-dark text-white'} border-1 fixed top-0 w-full z-10 transition-all duration-300`}>
             <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
                 <div className='relative flex h-20 items-center'>
-                    <div className='cursor-pointer'>
-                        <img className='h-8' src={logo} alt="" />
-                    </div>
+                    <a href='/' className='cursor-pointer'>
+                        <img className='h-8 hover:opacity-80 transition-opacity duration-300 ' src={logo} alt="Dev Kumar" />
+                    </a>
                     <div className='mx-auto relative hidden lg:block mr-4'>
                         <NavLink />
                     </div>
                     <div className='flex ml-auto'>
-                        <div className=''>
-                            <button className='bg-primary text-white rounded-full px-5 py-1 border-2 border-white hover:bg-secondary hover:text-black transition-all duration-300'>
+                        <div>
+                            <button className='bg-primary text-white rounded-full px-5 py-1 border-2 border-primary hover:bg-white hover:text-black transition-all duration-300'>
                                 <a href="/">Resume</a>
                             </button>
                         </div>
-                        <div className='ml-4'>
-                            <svg className={`${open ? "hidden" : "active"} lg:hidden h-8 cursor-pointer`} onClick={() => setOpen(true)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                        <div className='ml-4' onClick={() => setOpen(!open)}>
+                            {!open ? <svg className={` lg:hidden h-8 cursor-pointer`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
-                            </svg>
-                            <svg className={`${open ? "active" : "hidden"} hover:rotate-180 transition duration-300 lg:hidden h-8 cursor-pointer`} onClick={() => setOpen(false)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
+                            </svg> :
+                                <svg className={` hover:rotate-180 transition duration-300 lg:hidden h-8 cursor-pointer`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>}
                         </div>
                     </div>
                 </div>
             </div>
 
             {/* Navbar Mobile */}
-            <div className={`${open ? "left-[-1000px]" : "right-[1000px]"} lg:hidden bg-dark h-sceen transition-all duration-300`}>
+            <div className={`${open ? "fixed top-20 left-0 lg:hidden bg-dark h-sceen w-screen border-t border-purple-800 ease-in-out duration-300" : "fixed left-[-100%]"} `}>
                 <div className='space-y-1  '>
                     {open && <NavMob />}
                 </div>
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typed from "react-typed"
 
 const Hero = () => {
     return (
@@ -6,8 +7,18 @@ const Hero = () => {
             <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
                 <div className='flex flex-col items-center justify-center text-center h-screen gap-10 mt-5'>
                     <div className=''>
-                        <h6 className=' text-white opacity-60 font-medium text-2xl max-md:text-lg mb-3'>Hello, I'm Dev Kumar</h6>
-                        <h1 className='text-6xl max-md:text-4xl font-bold text-white mb-4 text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary'>UI/UX Developer</h1>
+                        <h6 className=' text-white opacity-60 font-medium text-2xl max-md:text-lg'>Hello, I'm Dev Kumar</h6>
+                        <div className='text-6xl max-md:text-4xl p-2 font-bold text-white mb-4 text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary'>
+                            <Typed
+                                strings={[
+                                    'Graphic Designer',
+                                    'Web Developer',
+                                ]}
+                                typeSpeed={60}
+                                backSpeed={60}
+                                loop
+                            />
+                        </div>
                         <p className='text-white mb-5 max-w-4xl max-md:text-sm'>Welcome to my portfolio, showcasing my skills in both graphic design and web development. From branding to website design, I have a passion for creating visually appealing and functional projects for my clients.</p>
                         <button className='bg-primary text-white px-4 py-1 rounded-full border-2 hover:bg-white hover:text-black '>Click me</button>
                     </div>
