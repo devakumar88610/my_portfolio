511eb4668c3ec817c84f5b50f4615ce4e0f70719
devakumar88610 <devakumar88610@gmail.com>
2023-04-15 23:56:00 +0530

    oki

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import Contact from './Components/Contact';
 import Header from './Navbar/Header';
 import Hero from './Components/Hero';
 import NavMob from './Navbar/NavMob'
-import OnePage from './Components/OnePage';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import ImageSlick from './Components/ImageSlick';
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Hero />
-      <ImageSlick />
-      <NavMob />
-      <Contact />
-      <Routes>
-        <Route exact path="id" element={<OnePage />} />
-      </Routes>
 
+      <NavMob />
+      <Contact />-
     </BrowserRouter>
     // <div className='bg-dark'>
     //   <Header />
